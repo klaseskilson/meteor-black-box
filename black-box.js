5ae65ec827283604f6674e766bb40a38b9535704
Klas Eskilson <klas.eskilson@gmail.com>
2015-08-24 19:02:25 +0200

    Add first wrapper version
    Calls to meteor method gets overwritten, and the methods get wrapped before
    they are added to the meteor method context.

--- black-box.js
+++ black-box.js
@@ -1,1 +1,31 @@
-// Write your package code here!
+var originalMethods = Meteor.methods;
+
+var _logger = {
+  func: console.info,
+  context: console
+};
+
+Meteor.methods = function (dict) {
+  var newDict = {};
+
+  _.each(dict, function (func, name) {
+    var wrappedFunction = _.wrap(func, function(innerFunc /*, arguments */) {
+      // slice away the first parameter
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      _logger.func.call(_logger.context, "Calling '" + name + "', arguments:", args);
+
+      var result = innerFunc.apply(this, args);
+      if (typeof result !== 'undefined') {
+        _logger.func.call(_logger.context, "Result from '" + name + "':", JSON.stringify(result));
+      }
+
+      _logger.func.call(_logger.context, "Ending '" + name + "' call");
+      return result;
+    });
+
+    newDict[name] = wrappedFunction;
+  });
+
+  originalMethods(newDict);
+};
