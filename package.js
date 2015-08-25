7953de291d1a13b02c6b2df917fe82fd39a90a57
Klas Eskilson <klas.eskilson@gmail.com>
2015-08-25 12:37:14 +0200

    Bump version after README update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'klaseskilson:black-box',
-  version: '0.0.2',
+  version: '0.0.2_1',
   // Brief, one-line summary of the package.
   summary: 'Log every Meteor.call',
   // URL to the Git repository containing the source code for this package.
